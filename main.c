c6e3235973505a6a6fb34bc9eb17124e735b1ca8
Mau Reyes <mauricio.reyes@rsmus.com>
2020-06-08 16:13:34 -0600

    lunes 08 reportes y agregar funciones restantes

--- main.c
+++ main.c
@@ -587,32 +587,123 @@ void crearRegistro(char *registro)
 //         system("pause>null");
 //     }
 // }
+// void mostrarRegistros(char *registro)
+// {
+//     if (strcmp(registro, USUARIO) == 0)
+//     {
+//         FILE *file;
+//         file = fopen(ARCHIVO_USUARIOS, "r");
+//         printf("\n\t\t\tREGISTROS DE USUARIOS EN EL SISTEMA\n");
+//         printf("\n");
+//         printf("*----------------------------------------------------------------------*\n");
+//         printf("|   ID   |       Nombre      |        Correo               |   acceso  |\n");
+//         printf("*----------------------------------------------------------------------*\n");
+//         while (fread(&Usuario, sizeof(Usuario), 1, file))
+//         {
+//             printf("\n| %i", Usuario.id);
+//             printf(" ");
+//             printf("\t | %s", Usuario.nombre);
+//             printf(" ");
+//             printf("\t     | %s", Usuario.correo);
+//             printf(" ");
+//             printf("\t           | %i", Usuario.acceso);
+
+//             printf(" ");
+//             printf("\t       |");
+//         }
+
+//         printf("\n*----------------------------------------------------------------------*\n");
+//         fclose(file);
+//         system("pause>null");
+//     }
+
+//     if (strcmp(registro, PELICULA) == 0)
+//     {
+//         FILE *file;
+//         file = fopen(ARCHIVO_PELICULAS, "r");
+
+//         printf("\n\t         REGISTROS DE PELICULAS EN EL SISTEMA\n");
+//         printf("\n");
+//         printf("*-----------------------------------------------------------------*\n");
+//         printf("|   ID   |               Titulo                 |        Genero   |\n");
+//         printf("*-----------------------------------------------------------------*\n");
+
+//         while (fread(&Pelicula, sizeof(Pelicula), 1, file))
+//         {
+//             printf("\n| %i", Pelicula.id);
+//             printf(" ");
+//             printf("\t |   %s", Pelicula.titulo);
+//             printf(" ");
+//             printf("\t\t\t\t|%s", Pelicula.genero);
+//             printf(" ");
+//             printf("\t  |");
+//         }
+
+//         printf("\n*-----------------------------------------------------------------*\n");
+//         fclose(file);
+//         system("pause>null");
+//     }
+
+//     if (strcmp(registro, SALA) == 0)
+//     {
+//         FILE *file;
+//         file = fopen(ARCHIVO_SALAS, "r");
+
+//         printf("\n\t         REGISTROS DE SALAS EN EL SISTEMA\n");
+//         printf("\n");
+//         printf("\t*---------------------------------------------*\n");
+//         printf("\t|    ID        |           Titulo             |\n");
+//         printf("\t*---------------------------------------------*\n");
+
+//         while (fread(&Sala, sizeof(Sala), 1, file))
+//         {
+//             printf("\t\t%i\t\t%s\n", Sala.id, Sala.pelicula);
+//         }
+//         printf("\t*---------------------------------------------*\n");
+//         fclose(file);
+//         system("pause>null");
+//     }
+// }
+
 void mostrarRegistros(char *registro)
 {
     if (strcmp(registro, USUARIO) == 0)
     {
         FILE *file;
         file = fopen(ARCHIVO_USUARIOS, "r");
-        printf("\n\t\t\tREGISTROS DE USUARIOS EN EL SISTEMA\n");
+        printf("\n\t\t\t   REGISTROS DE USUARIOS EN EL SISTEMA\n");
         printf("\n");
-        printf("*----------------------------------------------------------------------*\n");
-        printf("|   ID   |       Nombre      |        Correo               |   acceso  |\n");
-        printf("*----------------------------------------------------------------------*\n");
+        printf("*-----------------------------------------------------------------------------------------*\n");
+        printf("|   ID  |                Nombre                   |          Correo                       |\n");
+        printf("*-----------------------------------------------------------------------------------------*\n");
         while (fread(&Usuario, sizeof(Usuario), 1, file))
         {
+
+            int v = 0;
+            int u = 0;
+            int co = 0;
+            int acc = 0;
+
+            v = (log10(Usuario.id) + 1);
             printf("\n| %i", Usuario.id);
-            printf(" ");
-            printf("\t | %s", Usuario.nombre);
-            printf(" ");
-            printf("\t     | %s", Usuario.correo);
-            printf(" ");
-            printf("\t           | %i", Usuario.acceso);
+            for (v; v < 6; v++)
+            {
+                printf(" ");
+            }
+            printf("| %s", Usuario.nombre);
+            for (u = strlen(Usuario.nombre); u < 40; u++)
+            {
+                printf(" ");
+            }
+            printf("| %s", Usuario.correo);
+            for (co = strlen(Usuario.correo); co < 35; co++)
+            {
+                printf(" ");
+            }
 
-            printf(" ");
-            printf("\t       |");
+            printf("\t  |");
         }
-
-        printf("\n*----------------------------------------------------------------------*\n");
+        printf("\n*-----------------------------------------------------------------------------------------*\n");
         fclose(file);
         system("pause>null");
     }
@@ -622,24 +713,40 @@ void mostrarRegistros(char *registro)
         FILE *file;
         file = fopen(ARCHIVO_PELICULAS, "r");
 
-        printf("\n\t         REGISTROS DE PELICULAS EN EL SISTEMA\n");
+        printf("\n\t                   REGISTROS DE PELICULAS EN EL SISTEMA\n");
         printf("\n");
-        printf("*-----------------------------------------------------------------*\n");
-        printf("|   ID   |               Titulo                 |        Genero   |\n");
-        printf("*-----------------------------------------------------------------*\n");
+        printf("*---------------------------------------------------------------------------------*\n");
+        printf("|   ID  |                   Titulo                     |          Genero          |\n");
+        printf("*---------------------------------------------------------------------------------*\n");
 
         while (fread(&Pelicula, sizeof(Pelicula), 1, file))
         {
+
+            int p = 0;
+            int tp = 0;
+            int g = 0;
+
+            p = (log10(Pelicula.id) + 1);
             printf("\n| %i", Pelicula.id);
-            printf(" ");
-            printf("\t |   %s", Pelicula.titulo);
-            printf(" ");
-            printf("\t\t\t\t|%s", Pelicula.genero);
-            printf(" ");
-            printf("\t  |");
+            for (p; p < 6; p++)
+            {
+                printf(" ");
+            }
+            printf("| %s", Pelicula.titulo);
+            for (tp = strlen(Pelicula.titulo); tp < 45; tp++)
+            {
+                printf(" ");
+            }
+            printf("| %s", Pelicula.genero);
+            for (g = strlen(Pelicula.genero); g < 25; g++)
+            {
+                printf(" ");
+            }
+
+            printf("|");
         }
 
-        printf("\n*-----------------------------------------------------------------*\n");
+        printf("\n*---------------------------------------------------------------------------------*\n");
         fclose(file);
         system("pause>null");
     }
@@ -649,17 +756,17 @@ void mostrarRegistros(char *registro)
         FILE *file;
         file = fopen(ARCHIVO_SALAS, "r");
 
-        printf("\n\t         REGISTROS DE SALAS EN EL SISTEMA\n");
+        printf("\n\t             REGISTROS DE SALAS EN EL SISTEMA\n");
         printf("\n");
-        printf("\t*---------------------------------------------*\n");
-        printf("\t|    ID        |           Titulo             |\n");
-        printf("\t*---------------------------------------------*\n");
+        printf("\t*--------------------------------------------------------------*\n");
+        printf("\t|    ID        |                    Titulo                     |\n");
+        printf("\t*--------------------------------------------------------------*\n");
 
         while (fread(&Sala, sizeof(Sala), 1, file))
         {
             printf("\t\t%i\t\t%s\n", Sala.id, Sala.pelicula);
         }
-        printf("\t*---------------------------------------------*\n");
+        printf("\t*--------------------------------------------------------------*\n");
         fclose(file);
         system("pause>null");
     }
@@ -1421,7 +1528,6 @@ void mostrarReportes()
     // Mostrar tickets
     FILE *filetickets;
     filetickets = fopen(ARCHIVO_TICKETS, "r");
-    printf("\nfecha\t\tcantidadTickets\n\n");
     // obteniendo las fechas disponibles que estan en el reporte
     while (fread(&Ticket, sizeof(Ticket), 1, filetickets))
     {
@@ -1465,7 +1571,7 @@ void mostrarReportes()
     printf("\nFecha\t\t\tTickets vendidos\t\tDinero\n");
     for (i = 0; i <= cont; i++)
     {
-        printf("\n%s\t\t\t%i\t\t\t%.2lf", fechas[i], cantidadTicketsPorFechas[i], (cantidadTicketsPorFechas[i] * TICKET_PRECIO));
+        printf("\n%s\t\t\t%i\t\t\t$%.2lf", fechas[i], cantidadTicketsPorFechas[i], (cantidadTicketsPorFechas[i] * TICKET_PRECIO));
     }
 
     system("pause>null");
